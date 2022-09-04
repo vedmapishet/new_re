ca5b8cb82bfca0a98e1e94b4cc551185cd5a8b36
o.pendrak <o.pendrak@vk.team>
2022-09-04 17:25:43 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -1,7 +1,7 @@
 task_branch = "${TEST_BRANCH_NAME}"
 def branch = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch"
-git_base_url = "git@gitlab.com:epickonfetka/cicd-threadqa.git"
+git_base_url = 'git@gitlab.com:epickonfetka/cicd-threadqa.git'
 
 def execSh(script){
     sh(returnStdout: true, script: script)
@@ -9,13 +9,13 @@ def execSh(script){
 
 withEnv([ "branch=${branch}"]) {
     stage("Checkout Branch") {
-        if (!"$branch".contains("master")) {
+        if (!"$branch".contains('master')) {
             try {
-                execSh("""
+                execSh('''
                 git clone $git_base_url
                 git checkout $branch
                 git merge master
-                """)
+                ''')
 
             } catch (err) {
                 echo "Failed to merge master to branch $branch"
@@ -33,16 +33,16 @@ withEnv([ "branch=${branch}"]) {
 
 def testPart(){
     try {
-        execSh( "./gradlew clean testme")
+        execSh( './gradlew clean testme')
     } catch (err){
-        echo "some test are failed"
+        echo 'some test are failed'
         throw("${err}")
     } finally {
-        execSh("""
+        execSh('''
                 ./gradlew allureReport
                 zip -r report.zip build/reports/allure-report/allureReport/*
                 echo "Stage was finished"
-        """)
+        ''')
     }
 }
 
