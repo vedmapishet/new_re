23bb65af2e20d4e36e68422188f8869faa75d69b
o.pendrak <o.pendrak@vk.team>
2022-09-04 18:01:26 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -1,22 +1,22 @@
-//task_branch = "${TEST_BRANCH_NAME}"
-//def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-//currentBuild.displayName = "$branch_cutted"
+task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
 
 //withEnv([ "branch=${branch_cutted}"]) {
     stage("Checkout Branch") {
-      //  if (!"$branch_cutted".contains("master")) {
+        if (!"$branch_cutted".contains("master")) {
             try {
-                echo "Working with JIRA-4812-addLoginTest"
-                sh "git clone git@gitlab.com:epickonfetka/cicd-threadqa.git"
-                sh "git checkout JIRA-4812-addLoginTest"
-                sh "git merge master"
+                echo "Working with $branch_cutted"
+                shell("git clone git@gitlab.com:epickonfetka/cicd-threadqa.git")
+                shell("git checkout $branch_cutted")
+                shell("git merge master")
             } catch (err) {
-                echo "Failed to merge master to branch JIRA-4812-addLoginTest"
+                echo "Failed to merge master to branch $branch_cutted"
                 throw("${err}")
             }
-      //  } else {
-     //       echo "Current branch is master"
-     //   }
+        } else {
+            echo "Current branch is master"
+        }
     }
 
     stage("Run tests") {
@@ -26,13 +26,13 @@
 
 def testPart(){
     try {
-        sh "./gradlew clean testme"
+        shell("./gradlew clean testme")
     } catch (err){
         echo "some test are failed"
         throw("${err}")
     } finally {
-        sh "./gradlew allureReport"
-        sh "zip -r report.zip build/reports/allure-report/allureReport/*"
+        shell("./gradlew allureReport")
+        shell("zip -r report.zip build/reports/allure-report/allureReport/*")
         echo "Stage was finished"
     }
 }
