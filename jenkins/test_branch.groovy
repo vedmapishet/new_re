0487f520f9038e58878a9ec89c169f04a0ad87dc
o.pendrak <o.pendrak@vk.team>
2022-09-04 17:36:35 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -3,21 +3,17 @@ def branch = task_branch.contains("origin") ? task_branch.split('/')[1] : task_b
 currentBuild.displayName = "$branch"
 git_base_url = "git@gitlab.com:epickonfetka/cicd-threadqa.git"
 
-def execSh(script){
-    sh(returnStdout: true, script: script)
+def execSh(String script){
+    sh(returnStdout: true, script: script as GString)
 }
 
 withEnv([ "branch=${branch}"]) {
     stage("Checkout Branch") {
         if (!"$branch".contains("master")) {
             try {
-
-                execSh("""
-                git clone $git_base_url
-                git checkout $branch
-                git merge master
-                """.toString())
-
+                execSh("git clone $git_base_url")
+                execSh("git checkout $branch")
+                execSh("git merge master")
             } catch (err) {
                 echo "Failed to merge master to branch $branch"
                 throw("${err}")
