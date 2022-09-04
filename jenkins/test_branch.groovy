a1b5d530a2466cf3110c3a8d8407f5428b9eed17
o.pendrak <o.pendrak@vk.team>
2022-09-04 18:40:06 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -1,45 +1,47 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
+pipeline {
+    task_branch = "${TEST_BRANCH_NAME}"
+    def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+    currentBuild.displayName = "$branch_cutted"
 
-withEnv([ "branch=${branch_cutted}"]) {
-    stage("Checkout Branch") {
-        if (!"$branch_cutted".contains("master")) {
-            try {
-                labelledShell(label: 'Merge Master to Branch', script: '''
+    withEnv(["branch=${branch_cutted}"]) {
+        stage("Checkout Branch") {
+            if (!"$branch_cutted".contains("master")) {
+                try {
+                    labelledShell(label: 'Merge Master to Branch', script: '''
                   echo "Working with $branch"
                   git clone git@gitlab.com:epickonfetka/cicd-threadqa.git
                   git checkout $branch
                   git merge master
-                   '''.toString())
-            } catch (err) {
-                echo "Failed to merge master to branch $branch_cutted"
-                throw("${err}")
+                   ''')
+                } catch (err) {
+                    echo "Failed to merge master to branch $branch_cutted"
+                    throw ("${err}")
+                }
+            } else {
+                echo "Current branch is master"
             }
-        } else {
-            echo "Current branch is master"
         }
-    }
 
-    stage("Run tests") {
-        testPart()
+        stage("Run tests") {
+            testPart()
+        }
     }
 }
 
-def testPart(){
-    try {
-        labelledShell(label:'Run tests', script: '''
+    def testPart() {
+        try {
+            labelledShell(label: 'Run tests', script: '''
 .       /gradlew clean testme
         ''')
-    } catch (err){
-        echo "some test are failed"
-        throw("${err}")
-    } finally {
-        labelledShell(label:'Generate Allure Report', script: '''
+        } catch (err) {
+            echo "some test are failed"
+            throw ("${err}")
+        } finally {
+            labelledShell(label: 'Generate Allure Report', script: '''
             ./gradlew allureReport
             zip -r report.zip build/reports/allure-report/allureReport/*
             ''')
+        }
     }
-}
 
 
