e9398ef3fc24eec9bf2c7ff89e298943ece5ec06
o.pendrak <o.pendrak@vk.team>
2022-09-04 18:29:12 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -2,18 +2,16 @@ task_branch = "${TEST_BRANCH_NAME}"
 def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch_cutted"
 
-//withEnv([ "branch=${branch_cutted}"]) {
+withEnv([ "branch=${branch_cutted}"]) {
     stage("Checkout Branch") {
         if (!"$branch_cutted".contains("master")) {
             try {
-                echo "Working with $branch_cutted"
-
-                labelledShell(label: 'Build Private Apps', script: '''
-                echo "helolo"
+                labelledShell(label: 'Merge Master to Branch', script: '''
+                  echo "Working with $branch"
+                  git clone git@gitlab.com:epickonfetka/cicd-threadqa.git
+                  git checkout $branch
+                  git merge master
                    ''')
-                sh('git clone git@gitlab.com:epickonfetka/cicd-threadqa.git'.toString())
-                sh("git checkout $branch_cutted".toString())
-                sh('git merge master')
             } catch (err) {
                 echo "Failed to merge master to branch $branch_cutted"
                 throw("${err}")
@@ -26,18 +24,21 @@ currentBuild.displayName = "$branch_cutted"
     stage("Run tests") {
         testPart()
     }
-//}
+}
 
 def testPart(){
     try {
-        shell("./gradlew clean testme")
+        labelledShell(label:'Run tests', script: '''
+.       /gradlew clean testme
+        ''')
     } catch (err){
         echo "some test are failed"
         throw("${err}")
     } finally {
-        shell("./gradlew allureReport")
-        shell("zip -r report.zip build/reports/allure-report/allureReport/*")
-        echo "Stage was finished"
+        labelledShell(label:'Generate Allure Report', script: '''
+            ./gradlew allureReport
+            zip -r report.zip build/reports/allure-report/allureReport/*
+            ''')
     }
 }
 
