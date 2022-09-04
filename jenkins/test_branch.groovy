db529cd5c0e0151a2df46421ee5eebd767ecefff
o.pendrak <o.pendrak@vk.team>
2022-09-04 19:03:25 +0500

    JIRA-4812 add login tests

--- jenkins/test_branch.groovy
+++ jenkins/test_branch.groovy
@@ -2,40 +2,34 @@ task_branch = "${TEST_BRANCH_NAME}"
 def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch_cutted"
 
-pipeline {
-    agent any
-   // withEnv(["branch=${branch_cutted}"]) {
-        stages {
-            stage("Checkout Branch") {
-                script {
-                    if (!"$branch_cutted".contains("master")) {
-                        try {
-                            labelledShell(label: 'Merge Master to Branch', script: '''
-                  echo "Working with branch_cutted"
+withEnv(["branch=${branch_cutted}"]) {
+    stage("Checkout Branch") {
+        if (!"$branch_cutted".contains("master")) {
+            try {
+                labelledShell(label: 'Merge Master to Branch', script: '''
+                  echo "Working with $branch"
                   git clone git@gitlab.com:epickonfetka/cicd-threadqa.git
-                  git checkout branch_cutted
+                  git checkout $branch
                   git merge master
                    ''')
-                        } catch (err) {
-                            echo "Failed to merge master to branch $branch_cutted"
-                            throw ("${err}")
-                        }
-                    } else {
-                        echo "Current branch is master"
-                    }
-                }
+            } catch (err) {
+                echo "Failed to merge master to branch $branch_cutted"
+                throw ("${err}")
             }
-            stage("Run tests") {
-                testPart()
-            }
+        } else {
+            echo "Current branch is master"
         }
-  //  }
+    }
+
+    stage("Run tests") {
+        testPart()
+    }
 }
 
 def testPart() {
     try {
         labelledShell(label: 'Run tests', script: '''
-.       /gradlew clean testme
+            ./gradlew clean testme
         ''')
     } catch (err) {
         echo "some test are failed"
