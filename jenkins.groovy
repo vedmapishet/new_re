1587e4ad189d91cfe580948dd5add3c9ea30b06c
VIvanov <lolipop120999@gmail.com>
2022-09-16 21:29:02 +0500

    1

---
+++ jenkins.groovy
@@ -0,0 +1,99 @@
+task_branch = "${TEST_BRANCH_NAME}"
+def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
+currentBuild.displayName = "$branch_cutted"
+base_git_url = "https://gitlab.com/epickonfetka/cicd-threadqa.git"
+
+
+node {
+    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
+        stage("Checkout Branch") {
+            if (!"$branch_cutted".contains("master")) {
+                try {
+                    getProject("$base_git_url", "$branch_cutted")
+                    labelledShell(label: 'Merge Master to Branch', script: '''
+                  echo "Working with $branch"
+                  git clone $base_url
+                  git checkout origin/$branch
+                  git merge origin/master
+                  git push --all origin
+                  chmod +x gradlew
+                   ''')
+                } catch (err) {
+                    echo "Failed to merge master to branch $branch_cutted"
+                    throw ("${err}")
+                }
+            } else {
+                echo "Current branch is master"
+                getProject("$base_git_url", "$branch_cutted")
+            }
+        }
+
+//        try {
+//            parallel getTestStages(["apiTests", "uiTests"])
+//        } finally {
+//            stage ("Allure") {
+//                generateAllure()
+//            }
+//        }
+
+        try {
+            stage("Run tests") {
+                parallel(
+                        'Api Tests': {
+                            runTestWithTag("apiTests")
+                        },
+                        'Ui Tests': {
+                            runTestWithTag("uiTests")
+                        }
+                )
+            }
+        } finally {
+            stage("Allure") {
+                generateAllure()
+            }
+        }
+    }
+}
+
+
+def getTestStages(testTags) {
+    def stages = [:]
+    testTags.each { tag ->
+        stages["${tag}"] = {
+            runTestWithTag(tag)
+        }
+    }
+    return stages
+}
+
+
+def runTestWithTag(String tag) {
+    try {
+        labelledShell(label: "Run ${tag}", script: "./gradlew -x test ${tag}")
+    } finally {
+        echo "some failed tests"
+    }
+}
+
+def getProject(String repo, String branch) {
+    cleanWs()
+    checkout scm: [
+            $class           : 'GitSCM', branches: [[name: branch]],
+            userRemoteConfigs: [[
+                                        url: repo
+                                ]]
+    ]
+}
+
+def generateAllure() {
+    allure([
+            includeProperties: true,
+            jdk              : '',
+            properties       : [],
+            reportBuildPolicy: 'ALWAYS',
+            results          : [[path: 'build/allure-results']]
+    ])
+}
+
+
+
