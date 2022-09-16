3fce9999e000601327dfc21341d15fca96ab1062
VIvanov <lolipop120999@gmail.com>
2022-09-16 21:55:00 +0500

    new jenkins groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,7 @@
 task_branch = "${TEST_BRANCH_NAME}"
 def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch_cutted"
-base_git_url = "git@gitlab.com:epickonfetka/cicd-threadqa.git"
+base_git_url = "https://gitlab.com/epickonfetka/cicd-threadqa.git"
 
 
 node {
